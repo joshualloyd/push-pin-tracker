a6a05d55671d01a9e1eaf2dbf61ea248b3a230cd
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-08-07 16:08:05 -0500

    pins now display

--- app/factories/pin-factory.js
+++ app/factories/pin-factory.js
@@ -16,8 +16,27 @@ pushPinApp.factory('PinFactory', function($q, $http, FirebaseUrl) {
 
 	let getPin = () => {};
 
-	let getPins = () => {};
+	let getPins = (projectId) => {
+		return $q((resolve, reject) => {
+			$http.get(`${FirebaseUrl}pins.json`)
+			.then((pinsData) => {
+				let pinsArray = [];
+				let pinsObjects = pinsData.data;
+				Object.keys(pinsObjects).forEach((key) => {
+					pinsObjects[key].id = key;
+					pinsArray.push(pinsObjects[key]);
+				});
+				let filteredPinsArray = pinsArray.filter((pin) => {
+					return pin.project_id === projectId;
+				});
+				resolve(filteredPinsArray);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+		});
+	};
 
-	return {createPin};
+	return {createPin, getPins};
 
 });
